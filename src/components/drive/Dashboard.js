322362346e6d86cf868b7ced2148b7851537bb81
Kartik237 <kartik237@gmail.com>
2024-01-16 02:33:06 +0530

    Add Files and Upload track functionality added

--- src/components/drive/Dashboard.js
+++ src/components/drive/Dashboard.js
@@ -7,11 +7,15 @@ import AddFileButton from "./AddFileButton";
 import Folder from "./Folder";
 import { useParams, useLocation } from "react-router-dom";
 import FolderBreadcrumbs from "./FolderBreadcrumbs";
+import File from "./File";
 
 export default function Dashboard() {
   const { folderId } = useParams();
   const { state = {} } = useLocation();
-  const { folder, childFolders } = useFolder(folderId, state.folder);
+  const { folder, childFolders, childFiles } = useFolder(
+    folderId,
+    state.folder
+  );
 
   return (
     <>
@@ -22,7 +26,6 @@ export default function Dashboard() {
           <AddFileButton currentFolder={folder} />
           <AddFolderButton currentFolder={folder} />
         </div>
-
         {childFolders.length > 0 && (
           <div className="d-flex flex-wrap">
             {childFolders.map((childFolder) => (
@@ -32,6 +35,20 @@ export default function Dashboard() {
                 className="p-2"
               >
                 <Folder folder={childFolder} />
+              </div>
+            ))}
+          </div>
+        )}
+        {childFolders.length > 0 && childFiles.length > 0 && <hr />}
+        {childFiles.length > 0 && (
+          <div className="d-flex flex-wrap">
+            {childFiles.map((childFile) => (
+              <div
+                key={childFile.id}
+                style={{ maxWidth: "250px" }}
+                className="p-2"
+              >
+                <File file={childFile} />
               </div>
             ))}
           </div>
--- src/components/hooks/useFolder.js
+++ src/components/hooks/useFolder.js
@@ -6,6 +6,7 @@ const ACTIONS = {
   SELECT_FOLDER: "select-folder",
   UPDATE_FOLDER: "update-folder",
   SET_CHILD_FOLDERS: "set-child-folders",
+  SET_CHILD_FILES: "set-child-files",
 };
 
 export const ROOT_FOLDER = { name: "Root", id: null, path: [] };
@@ -16,10 +17,9 @@ function reducer(state, { type, payload }) {
       return {
         folderId: payload.folderId,
         folder: payload.folder,
-        childFolders: [],
         childFiles: [],
+        childFolders: [],
       };
-
     case ACTIONS.UPDATE_FOLDER:
       return {
         ...state,
@@ -29,6 +29,11 @@ function reducer(state, { type, payload }) {
       return {
         ...state,
         childFolders: payload.childFolders,
+      };
+    case ACTIONS.SET_CHILD_FILES:
+      return {
+        ...state,
+        childFiles: payload.childFiles,
       };
     default:
       return state;
@@ -43,6 +48,7 @@ export function useFolder(folderId = null, folder = null) {
     childFiles: [],
   });
   const { currentUser } = useAuth();
+
   useEffect(() => {
     dispatch({ type: ACTIONS.SELECT_FOLDER, payload: { folderId, folder } });
   }, [folderId, folder]);
@@ -64,26 +70,41 @@ export function useFolder(folderId = null, folder = null) {
           payload: { folder: database.formatDoc(doc) },
         });
       })
-      .catch((e) => {
-        console.error(e);
+      .catch(() => {
         dispatch({
           type: ACTIONS.UPDATE_FOLDER,
           payload: { folder: ROOT_FOLDER },
         });
       });
-  }, [folderId]); //only effects when folder id changes
+  }, [folderId]);
 
   useEffect(() => {
     return database.folders
       .where("parentId", "==", folderId)
       .where("userId", "==", currentUser.uid)
       .orderBy("createdAt")
-      .onSnapshot(snapshot => {
+      .onSnapshot((snapshot) => {
         dispatch({
           type: ACTIONS.SET_CHILD_FOLDERS,
           payload: { childFolders: snapshot.docs.map(database.formatDoc) },
+        });
+      });
+  }, [folderId, currentUser]);
+
+  useEffect(() => {
+    return (
+      database.files
+        .where("folderId", "==", folderId)
+        .where("userId", "==", currentUser.uid)
+        // .orderBy("createdAt")
+        .onSnapshot((snapshot) => {
+          dispatch({
+            type: ACTIONS.SET_CHILD_FILES,
+            payload: { childFiles: snapshot.docs.map(database.formatDoc) },
+          });
         })
-      })
-  }, [folderId, currentUser])
+    );
+  }, [folderId, currentUser]);
+
   return state;
 }
