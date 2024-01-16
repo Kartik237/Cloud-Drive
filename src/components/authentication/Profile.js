139f79eb0d655873946ded917bd0c2af448e5b3a
Kartik Mishra <kartik237@gmail.com>
2024-01-16 10:34:01 +0530

    css chganes 2

--- src/components/authentication/Profile.js
+++ src/components/authentication/Profile.js
@@ -21,22 +21,29 @@ export default function Profile() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
-      </div>
-    </CenteredContainer>
+    <div
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1612670940073-8aed2145ebc3?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ')",
+      }}
+    >
+      <CenteredContainer>
+        <Card className="card border-primary">
+          <Card.Body>
+            <h2 className="text-center mb-4">Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email:</strong> {currentUser.email}
+            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+              Update Profile
+            </Link>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Button variant="link" onClick={handleLogout}>
+            Log Out
+          </Button>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
