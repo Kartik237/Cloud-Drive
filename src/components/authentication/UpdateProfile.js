131ca8b118f058f92dc53e7a3da8724f09ba7784
Kartik Mishra <kartik237@gmail.com>
2024-01-16 10:30:58 +0530

    CSS Changes

--- src/components/authentication/UpdateProfile.js
+++ src/components/authentication/UpdateProfile.js
@@ -43,46 +43,57 @@ export default function UpdateProfile() {
   }
 
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Update Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                defaultValue={currentUser.email}
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Update
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Link to="/user">Cancel</Link>
-      </div>
-    </CenteredContainer>
+    <div
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1612670940073-8aed2145ebc3?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ')",
+      }}
+    >
+      <CenteredContainer>
+        <Card className="card border-primary">
+          <Card.Body>
+            <h2 className="text-center mb-4">Update Profile</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  ref={emailRef}
+                  required
+                  defaultValue={currentUser.email}
+                />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordRef}
+                  placeholder="Leave blank to keep the same"
+                />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control
+                  type="password"
+                  ref={passwordConfirmRef}
+                  placeholder="Leave blank to keep the same"
+                />
+              </Form.Group>
+              <Button
+                disabled={loading}
+                className="w-100 outlin-primary"
+                type="submit"
+              >
+                Update
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          <Link to="/user">Cancel</Link>
+        </div>
+      </CenteredContainer>
+    </div>
   );
 }
--- src/components/drive/Navbar.js
+++ src/components/drive/Navbar.js
@@ -2,16 +2,23 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import drive from "../../../src/assets/drive.svg";
+import User from "../../../src/assets/User.png";
 export default function NavbarComponent() {
   return (
     <Navbar bg="light" expand="sm">
       <Navbar.Brand as={Link} to="/">
-        <img src={drive} alt="Drive Icon" style={{height:50}}/> Cloud Drive {/* Use the drive icon in the Navbar */}
+        <img src={drive} alt="Drive Icon" style={{ height: 50 }} /> Cloud Drive{" "}
+        {/* Use the drive icon in the Navbar */}
       </Navbar.Brand>
-      <Nav>
+      <Nav class="navbar-nav ml-auto">
         <Nav.Link as={Link} to="/user">
           Profile
-        </Nav.Link> 
+          <img
+            src={User}
+            alt="Drive Icon"
+            style={{ height: 40, marginLeft: 10 }}
+          />
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
